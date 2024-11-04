d3f8fa723115bdacca4bc40c7c1e5fe49809e035
chetannaidu2005 <126077917+chetannaidu2005@users.noreply.github.com>
2024-11-04 22:36:06 +0530

    Update Labs.java

--- Labs.java
+++ Labs.java
@@ -1,8 +1,8 @@
-import java.time.LocalDateTime;
 import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class Labs {
-    private Patient patient;
+    protected Patient patient;
     private Doctor doctor;
     private HospitalStaff hospitalStaff;
    private  Equipment equipment;
