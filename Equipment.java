784e477601312f414ae09b45327cec7729f928fb
chetannaidu2005 <126077917+chetannaidu2005@users.noreply.github.com>
2024-11-09 11:45:42 +0530

    Update Equipment.java

--- Equipment.java
+++ Equipment.java
@@ -9,8 +9,6 @@ public class Equipment {
     Date dateOfPurchase;
     Date dateOfLastService;
     String serviceRecord;
-
-    // Additional attributes
     String manufacturer;
     String serialNumber;
     Date warrantyExpirationDate;
@@ -19,12 +17,26 @@ public class Equipment {
     double operatingHours;
     String condition;
     Date calibrationDate;
-    List <UsageLog> usageLogs; // Log of each usage
-    Insurance insuranceDetails; // Custom class for insurance
+    List<UsageLog> usageLogs;
+    Insurance insuranceDetails;
     Date decommissionDate;
-    public String getManufacturer() { 
-        return manufacturer; 
+
+    // Default constructor
+    public Equipment() {}
+
+    // Parameterized constructor
+    public Equipment(String equipmentName, String equipmentType, String department, HospitalStaff staff, Date dateOfPurchase, Date dateOfLastService, String serviceRecord) {
+        this.equipmentName = equipmentName;
+        this.equipmentType = equipmentType;
+        this.department = department;
+        this.staff = staff;
+        this.dateOfPurchase = dateOfPurchase;
+        this.dateOfLastService = dateOfLastService;
+        this.serviceRecord = serviceRecord;
     }
+
+    // Additional Getters and Setters
+    public String getManufacturer() { return manufacturer; }
     public void setManufacturer(String manufacturer) { this.manufacturer = manufacturer; }
 
     public String getSerialNumber() { return serialNumber; }
@@ -36,24 +48,9 @@ public class Equipment {
     public double getMaintenanceCost() { return maintenanceCost; }
     public void setMaintenanceCost(double maintenanceCost) { this.maintenanceCost = maintenanceCost; }
 
-    public String getLocation() { return location; }
-    public void setLocation(String location) { this.location = location; }
-
-    public double getOperatingHours() { return operatingHours; }
-    public void setOperatingHours(double operatingHours) { this.operatingHours = operatingHours; }
-
     public String getCondition() { return condition; }
     public void setCondition(String condition) { this.condition = condition; }
 
-    public Date getCalibrationDate() { return calibrationDate; }
-    public void setCalibrationDate(Date calibrationDate) { this.calibrationDate = calibrationDate; }
-
-    public List<UsageLog> getUsageLogs() { return usageLogs; }
-    public void setUsageLogs(List<UsageLog> usageLogs) { this.usageLogs = usageLogs; }
-
-    public Insurance getInsuranceDetails() { return insuranceDetails; }
-    public void setInsuranceDetails(Insurance insuranceDetails) { this.insuranceDetails = insuranceDetails; }
-
-    public Date getDecommissionDate() { return decommissionDate; }
-    public void setDecommissionDate(Date decommissionDate) { this.decommissionDate = decommissionDate; }
+    public double getOperatingHours() { return operatingHours; }
+    public void setOperatingHours(double operatingHours) { this.operatingHours = operatingHours; }
 }
